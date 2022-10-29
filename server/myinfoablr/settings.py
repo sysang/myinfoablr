3ef204dfd857f3e6416c71669bee7378bae2e973
sysang <daosysang@gmail.com>
2022-10-29 08:35:54 +0700

    Fix static serving settings

--- server/myinfoablr/settings.py
+++ server/myinfoablr/settings.py
@@ -104,10 +104,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/media'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = '/var/static'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
